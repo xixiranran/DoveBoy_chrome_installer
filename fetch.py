201b152f1b4bd5c68830ee449b853f437a2f9232
熙然 <970852638@qq.com>
2024-04-13 00:53:51 +0800

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -3,10 +3,10 @@ import requests
 url = "https://download.mozilla.org/?product=firefox-latest&os=Win&lang=zh-CN"
 response = requests.get(url)
 
+print(response)
 # 检查响应状态码
 if response.status_code == 200:
     # 处理响应内容，获取下载链接
-    print(response)
     download_link = response.url
     print(download_link)
 else:
